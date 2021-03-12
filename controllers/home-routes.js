ba0671590a5e26e419bff3be1129bf51fb53a500
Ben Tan <btan87@gmail.com>
2021-03-11 17:58:19 -0800

    14.3.1

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,11 +2,6 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment, Vote } = require('../models');
 
-
-router.get('/login', (req, res) => {
-  res.render('login');
-});
-
 // get all posts for homepage
 router.get('/', (req, res) => {
   console.log('======================');
@@ -42,6 +37,15 @@ router.get('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
